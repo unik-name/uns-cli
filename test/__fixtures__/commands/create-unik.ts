f318653c97e515e29a80fa6f962b1910ddc3c79b
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-10-31 16:15:40 +0100

    fix: fix getVersion network version getter

--- test/__fixtures__/commands/create-unik.ts
+++ test/__fixtures__/commands/create-unik.ts
@@ -18,6 +18,7 @@ export const transaction = {
     timestamp: TRANSACTION_TIMESTAMP,
     version: 1,
     type: 11,
+    network: 30,
     fee: 100000000,
     senderPublicKey: "020d5e36cce37494811c1a6d8c5e05f744f45990cbcc1274d16914e093a5061011",
     amount: 0,
