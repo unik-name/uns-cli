68a126d784a7ff418b18bf9aab0e2055c85c887c
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-01-08 20:15:40 +0100

    feat: add vote command tests

---
+++ test/commands/delegate/vote.test.ts
@@ -0,0 +1,23 @@
+import * as SDK from "@uns/ts-sdk";
+import { commandName, meta, shouldExit, wallet } from "../../__fixtures__/commands/delegate/vote";
+import { applyExitCase } from "../../__fixtures__/commons";
+
+describe(`${commandName} command`, () => {
+    beforeEach(() => {
+        process.env.DEV_MODE = "true";
+    });
+
+    describe("Exit cases", () => {
+        jest.spyOn(SDK, "didResolve").mockImplementation(() => {
+            return new Promise((resolve, _) => {
+                resolve({
+                    data: wallet.address,
+                    chainmeta: meta,
+                    confirmations: 12,
+                });
+            });
+        });
+
+        shouldExit.forEach(exitCase => applyExitCase(exitCase));
+    });
+});
