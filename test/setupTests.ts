881d47208f5a2a31ecd1aea484f171a0fe8ca2f1
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-01-09 09:20:29 +0100

    feat: remove listeners after each tests to remove warrning from stdout

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,3 +3,9 @@ beforeEach(() => {
     process.env.UNS_NETWORK = undefined;
     process.env.DEV_MODE = undefined;
 });
+
+afterEach(() => {
+    // Launching all tests throws `MaxListenersExceededWarning: Possible EventEmitter memory leak detected` warning. In some tests we test stdout content, we can't have warnings
+    process.stdout.removeAllListeners();
+    process.stderr.removeAllListeners();
+});
