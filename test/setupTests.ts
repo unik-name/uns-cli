869db1bd21efad504cdfde6fa87f4f12e7917b7d
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-01-28 11:52:41 +0100

    test: temp skip

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,3 +1,5 @@
+import { NftMintDemandCertificationSigner } from "@uns/crypto";
+
 beforeEach(() => {
     // Reset all environment variables, which might be configured on this computer
     process.env.UNS_NETWORK = undefined;
@@ -8,4 +10,15 @@ afterEach(() => {
     // Launching all tests throws `MaxListenersExceededWarning: Possible EventEmitter memory leak detected` warning. In some tests we test stdout content, we can't have warnings
     process.stdout.removeAllListeners();
     process.stderr.removeAllListeners();
+});
+
+beforeAll(async () => {
+    // Handle ECDSA curve init ()
+    try {
+        await new NftMintDemandCertificationSigner({ iss: "", sub: "", iat: 123 }).sign(
+            "path adult harsh access case slice source error protect brave reason okay",
+        );
+    } catch (_) {
+        /* Nothing to do */
+    }
 });
