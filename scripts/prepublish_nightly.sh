54b6f7c480d8d2a79651c725e096e766ff1ace59
Damien Lecan <damien.lecan@spacelephant.org>
2020-02-02 10:10:22 +0100

    ci: fix nightly build with a more robust variable handling

--- scripts/prepublish_nightly.sh
+++ scripts/prepublish_nightly.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-echo "Upgrade @uns/ts-sdk dependency."
 sdk_version=$(grep '"@uns/ts-sdk":' package.json | cut -d\" -f4)
+
+echo "Configured version of UNS SDK: ${sdk_version}"
+
+echo "Upgrade @uns/ts-sdk dependency..."
 yarn remove @uns/ts-sdk
-yarn add @uns/ts-sdk@$sdk_version
+yarn add @uns/ts-sdk@"${sdk_version}"
